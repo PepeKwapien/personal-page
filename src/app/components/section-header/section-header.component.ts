f0e34dfa8118203bc70dd1de3f90160b896dd70c
PepeKwapien <pppkwapien@gmail.com>
2023-06-04 14:26:20 +0200

    Greeting based on the time of the day

--- src/app/components/section-header/section-header.component.ts
+++ src/app/components/section-header/section-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Images } from 'src/app/enums/images.enum';
+import { coerceBooleanProperty } from 'src/app/helpers/boolean.helper';
 
 @Component({
     selector: 'app-section-header[sectionHeaderMessage]',
@@ -7,9 +8,34 @@ import { Images } from 'src/app/enums/images.enum';
     styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent {
-    @Input() sectionHeaderMessage: string = "Hello, I'm Paweł Kwapień";
+    private _showGreeting: boolean = false;
+    @Input()
+    get showGreeting() {
+        return this._showGreeting;
+    }
+    set showGreeting(value: boolean) {
+        this._showGreeting = coerceBooleanProperty(value);
+    }
+
+    @Input() sectionHeaderMessage: string = "I'm Paweł Kwapień";
     @Input() backgroundImage: Images = Images.florence;
 
     constructor() {}
+
+    get greeting(): string {
+        const dateNow = new Date();
+        const hourNow = dateNow.getHours();
+        let greeting;
+
+        if (hourNow >= 3 && hourNow < 12) {
+            greeting = 'Good morning ☕';
+        } else if (hourNow >= 12 && hourNow < 19) {
+            greeting = 'Good afternoon 👋';
+        } else {
+            greeting = 'Good evening 😴';
+        }
+
+        return greeting;
+    }
 }
 
