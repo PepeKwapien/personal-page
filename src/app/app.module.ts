e08ab8c995a6686610a80592a89df437d2fad309
PepeKwapien <pppkwapien@gmail.com>
2023-06-04 15:18:23 +0200

    Revert "Fontawesome"
    This reverts commit 2ed4d8270b69c76cc87d3a7ba4fbd416982b5c1e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { SectionHeaderComponent } from './components/section-header/section-header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-    declarations: [AppComponent, SectionHeaderComponent, NavbarComponent],
-    imports: [BrowserModule, FontAwesomeModule],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    SectionHeaderComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
