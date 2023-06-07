fef44dc86cf710512cfbf5b4a07211fae22ed0fb
PepeKwapien <pppkwapien@gmail.com>
2023-06-08 01:52:15 +0200

    Progress bar input

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -6,7 +6,20 @@ import { Component, Input, OnInit } from '@angular/core';
     styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent implements OnInit {
-    @Input() progress: number = 0;
+    private _progress = 0;
+    @Input()
+    get progress(): number {
+        return this._progress;
+    }
+    set progress(value: number) {
+        if (value < 0) {
+            this._progress = 0;
+        } else if (value > 100) {
+            this._progress = 100;
+        } else {
+            this._progress = value;
+        }
+    }
 
     constructor() {}
 
