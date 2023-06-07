21b736be4ac2cb8d14469ab3cc05ef2297774dcf
PepeKwapien <pppkwapien@gmail.com>
2023-06-07 21:27:54 +0200

    No html variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { SectionHeaderComponent } from './components/section-header/section-header.component';
 import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
-    declarations: [AppComponent, LandingComponent, NavbarComponent, AboutMeComponent, SectionHeaderComponent, SkillsComponent],
+    declarations: [AppComponent, LandingComponent, AboutMeComponent, SectionHeaderComponent, SkillsComponent],
     imports: [BrowserModule, FontAwesomeModule],
     providers: [],
     bootstrap: [AppComponent]
