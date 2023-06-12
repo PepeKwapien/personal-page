aeccb84d4834867aeb91beeb1f37de7707400827
PepeKwapien <pppkwapien@gmail.com>
2023-06-12 12:02:56 +0200

    Moved intersection observer from progress bar to skills

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -1,11 +1,11 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-progress-bar',
     templateUrl: './progress-bar.component.html',
     styleUrls: ['./progress-bar.component.scss']
 })
-export class ProgressBarComponent implements AfterViewInit {
+export class ProgressBarComponent {
     private _progress = 0;
     @Input()
     get progress(): number {
@@ -22,20 +22,5 @@ export class ProgressBarComponent implements AfterViewInit {
     }
 
     constructor() {}
-
-    ngAfterViewInit() {
-        const intersectionObserver = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.remove('zero-width');
-                } else {
-                    entry.target.classList.add('zero-width');
-                }
-            });
-        });
-
-        const blurElements = document.querySelectorAll('.zero-width');
-        blurElements.forEach((el) => intersectionObserver.observe(el));
-    }
 }
 
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Images } from 'src/app/enums/images.enum';
 import { personalTraits, personalTraitsOrdered } from 'src/app/helpers/personal-traits.collection';
 import { technicalSkillsOrdered } from 'src/app/helpers/technical-skills.collection';
@@ -9,11 +9,26 @@ import { ITechnicalSkill } from 'src/app/interfaces/technical-skill.interface';
     templateUrl: './skills.component.html',
     styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent {
+export class SkillsComponent implements AfterViewInit {
     public image: Images = Images.skills;
     public skills: ITechnicalSkill[] = technicalSkillsOrdered;
     public traits: string[] = personalTraitsOrdered;
 
     constructor() {}
+
+    ngAfterViewInit() {
+        const intersectionObserver = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.remove('zero-width');
+                } else {
+                    entry.target.classList.add('zero-width');
+                }
+            });
+        });
+
+        const blurElements = document.querySelectorAll('.zero-width');
+        blurElements.forEach((el) => intersectionObserver.observe(el));
+    }
 }
 
