eb43f3e228a44bf38f7b7a5934d6873bcaafbc4b
PepeKwapien <pppkwapien@gmail.com>
2023-06-12 17:41:37 +0200

    Use webp

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -21,10 +21,9 @@ export class SkillsComponent implements AfterViewInit {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
                     entry.target.classList.remove('zero-width');
+                } else {
+                    entry.target.classList.add('zero-width');
                 }
-                // else {
-                //     entry.target.classList.add('zero-width');
-                // }
             });
         });
 
