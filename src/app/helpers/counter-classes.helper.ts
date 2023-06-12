3aea8cd54e77c3d45bf42e6d039e21dad8f85eac
PepeKwapien <pppkwapien@gmail.com>
2023-06-12 20:16:11 +0200

    Removed one reundant class

--- src/app/helpers/counter-classes.helper.ts
+++ src/app/helpers/counter-classes.helper.ts
@@ -2,6 +2,5 @@ export const counterClasses: Map<string, string> = new Map([
     ['blur', 'unblur'],
     ['hide-right', 'show-horizontal'],
     ['hide-left', 'show-horizontal'],
-    ['move-down', 'move-up'],
-    ['max-width', 'no-max-width']
+    ['move-down', 'move-up']
 ]);
