f63356fab2f812d794966de36e47da84649ebeaf
PepeKwapien <pppkwapien@gmail.com>
2023-11-02 23:12:54 +0100

    Updated interests

--- src/app/helpers/interests.collection.ts
+++ src/app/helpers/interests.collection.ts
@@ -3,8 +3,9 @@ import { IInterest } from '../interfaces/interest.interface';
 export const interests: IInterest[] = [
     { emoji: '🐉', description: 'fantasy reader' },
     { emoji: '🗾', description: 'japanese learner' },
-    { emoji: '🏋️', description: 'gym goer' },
-    { emoji: '🐱', description: 'cat lover' },
+    { emoji: '🏋️', description: 'body builder' },
     { emoji: '🧘‍♂️', description: 'meditation enthusiast' },
-    { emoji: '⚔️', description: 'witcher enjoyer' }
+    { emoji: '⚔️', description: 'witcher enjoyer' },
+    { emoji: '🐱', description: 'cat lover' },
+    { emoji: '🕷️', description: 'friendly neighborhood Spider-Man' }
 ];
